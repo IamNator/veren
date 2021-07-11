6c46a764c3bf9a0041609996bdaa9d6b93b5ff93
IamNator <natverior1@gmail.com>
2021-07-11 10:29:44 +0100

    ...

--- internal/controllers/veiw.go
+++ internal/controllers/veiw.go
@@ -1,10 +1,14 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+)
 
 //HomePageHandler serves the first page of the application
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 
-	http.ServeFile(w, r, "/frontend/dist")
+	//	http.ServeFile(w, r, "/frontend/dist/index.html")
+
+	http.ServeFile(w, r, "./frontend/dist")
 
 }
